4c0f1b2a0e15ffa737393ebf714c3253aefce505
Anisha <anishagc83@gmail.com>
2025-05-29 11:16:29 +0545

    first commit

--- app/src/main/java/com/example/plantstore/NavigationActivity.kt
+++ app/src/main/java/com/example/plantstore/NavigationActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,8 +21,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.plantstore.ui.theme.PlantStoreTheme
 
@@ -38,6 +45,16 @@ class NavigationActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavigationBody(){
+    data class BottomNavItem(val label: String, val icon: ImageVector)
+
+    val bottomNavItems = listOf(
+        BottomNavItem("Home", Icons.Default.Home),
+        BottomNavItem("Search", Icons.Default.Search),
+        BottomNavItem("Profile", Icons.Default.Person)
+    )
+
+    var selectedIndex by remember{ mutableStateOf(0) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -65,17 +82,17 @@ fun NavigationBody(){
                 },
 
 
-                navigationIcon = {
-                    IconButton( onClick = {}) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = null
-
-                        )
-                    }
-
-
-                }
+//                navigationIcon = {
+//                    IconButton( onClick = {}) {
+//                        Icon(
+//                            imageVector = Icons.Default.ArrowBack,
+//                            contentDescription = null
+//
+//                        )
+//                    }
+//
+//
+//                }
             )
         }
     ){
