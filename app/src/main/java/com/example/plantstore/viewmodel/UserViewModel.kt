19e5ad66fe3a6085c5d6e5c24f30c10960fb1aa1
Anisha <anishagc83@gmail.com>
2025-07-19 12:07:06 +0545

    Add ViewModels for managing user and product data

---
+++ app/src/main/java/com/example/plantstore/viewmodel/UserViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.plantstore.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.plantstore.model.UserModel
+import com.example.plantstore.repository.UserRepo
+import com.google.firebase.auth.FirebaseUser
+import androidx.lifecycle.LiveData
+
+
+class UserViewModel(val repo: UserRepo): ViewModel() {
+    fun login(
+        email: String, password: String,
+        callback: (Boolean, String) -> Unit
+    ){
+        repo.login(email,password,callback)
+    }
+
+    //authentication function
+    fun register(
+        email: String, password: String,
+        callback: (Boolean, String, String) -> Unit
+    ){
+        repo.register(email, password, callback)
+    }
+
+    //database function
+    fun addUserToDatabase(
+        userId: String, model: UserModel,
+        callback: (Boolean, String) -> Unit
+    ){
+        repo.addUserToDatabase(userId, model,callback)
+    }
+
+    fun updateProfile(userId: String,data : MutableMap<String,Any?>,
+                      callback: (Boolean, String) -> Unit){
+        repo.updateProfile(userId,data,callback)
+    }
+
+    fun forgetPassword(
+        email: String, callback: (Boolean, String) -> Unit
+    ){
+        repo.forgetPassword(email, callback)
+    }
+
+    fun getCurrentUser(): FirebaseUser?{
+        return  repo.getCurrentUser()
+    }
+
+    private  val _users = MutableLiveData<UserModel?>()
+    val users: LiveData<UserModel?> get() = _users
+
+    fun getUserById(
+        userId: String,
+    ){
+        repo.getUserById(userId){ users,success,message ->
+            if (success && users != null){
+                _users.postValue(users)
+            }else{
+                _users.postValue(null)
+            }
+        }
+         
+    }
+
+    fun logout(callback: (Boolean, String) -> Unit){
+        repo.logout(callback)
+    }
+}
