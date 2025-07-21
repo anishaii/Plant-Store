185d0aa46ad54fd72b9bd35c78ff69efd1ced863
Anisha <anishagc83@gmail.com>
2025-07-21 17:23:07 +0545

    ProductViewModel.kt added

---
+++ app/src/main/java/com/example/plantstore/viewmodel/ProductViewModel.kt
@@ -0,0 +1,72 @@
+package com.example.plantstore.viewmodel
+
+import android.content.Context
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.plantstore.model.ProductModel
+import com.example.plantstore.repository.ProductRepo
+
+class ProductViewModel(val repo: ProductRepo) : ViewModel(){
+    fun uploadImage(context: Context,imageUri: Uri, callback: (String?) -> Unit){
+        repo.uploadImage(context,imageUri,callback)
+    }
+
+    fun addProduct(
+        model: ProductModel,
+        callback: (Boolean, String) -> Unit
+    ){repo.addProduct(model,callback)}
+
+    fun updateProduct(
+        productId: String,
+        data: MutableMap<String, Any?>,
+        callback: (Boolean, String) -> Unit
+    ){
+        repo.updateProduct(productId,data,callback)
+    }
+
+    fun deleteProduct(
+        productId: String,
+        callback: (Boolean, String) -> Unit
+    ){
+        repo.deleteProduct(productId ,callback)
+    }
+
+    private  val  _products = MutableLiveData<ProductModel?>()
+    val products : LiveData<ProductModel?> get() = _products
+
+    private val _allProducts = MutableLiveData<List<ProductModel?>>()
+    val allProducts: LiveData<List<ProductModel?>> get() = _allProducts
+
+    fun getProductById(productId: String){
+        repo.getProductById(productId) { success, msg, data ->
+            if (success) {
+                _products.postValue(data)
+            } else {
+                _products.postValue(null)
+
+            }
+        }
+    }
+
+    private  val _loading = MutableLiveData<Boolean>()
+    var loading = MutableLiveData<Boolean>()
+        get() = _loading
+
+    fun getAllProduct(){
+        _loading.postValue(true)
+        repo.getAllProduct  { success, msg, data ->
+            if (success) {
+                _loading.postValue(false)
+                _allProducts.postValue(data)
+            } else {
+                _loading.postValue(false)
+                _allProducts.postValue(emptyList())
+
+            }
+        }
+    }
+
+
+}
